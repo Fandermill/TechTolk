2eef79ebabb4d086ba4fd3902bc7180ca6f8b396
Folkert van der Molen <folkert.vd.molen@gmail.com>
2024-01-18 21:59:39 +0100

    allow translation set source types with parameterless constructors without registered factory services

--- src/TechTolk/Sources/Internals/TranslationSetSourceFactory.cs
+++ src/TechTolk/Sources/Internals/TranslationSetSourceFactory.cs
@@ -15,9 +15,28 @@ internal class TranslationSetSourceFactory : ITranslationSetSourceFactory
     public ITranslationSetSource Create(ResolveSourceRegistration sourceRegistration)
     {
         var closedTypeSourceFactoryType = typeof(ITranslationSetSourceFactory<>).MakeGenericType(sourceRegistration.Type);
-        var factoryType = _serviceProvider.GetService(closedTypeSourceFactoryType) as ITranslationSetSourceFactory
-            ?? throw new RegistrationException($"There is no registered factory type for source type '{sourceRegistration.Type.Name}'");
+        var factory = _serviceProvider.GetService(closedTypeSourceFactoryType) as ITranslationSetSourceFactory;
+        if (factory is null)
+        {
+            if (HasParameterlessConstructor(sourceRegistration.Type))
+            {
+                return Activator.CreateInstance(sourceRegistration.Type, false) as ITranslationSetSource
+                    ?? throw new RegistrationException(
+                        $"Unable to instantiate '{sourceRegistration.Type.Name}' through it's parameterless " +
+                        $"constructor. Please register a '{nameof(ITranslationSetSourceFactory)}' for it.");
+            }
+            else
+            {
+                throw new RegistrationException(
+                    $"There is no registered factory type for source type '{sourceRegistration.Type.Name}'");
+            }
+        }
 
-        return factoryType.Create(sourceRegistration);
+        return factory.Create(sourceRegistration);
+    }
+
+    private static bool HasParameterlessConstructor(Type type)
+    {
+        return type.GetConstructor(Type.EmptyTypes) is not null;
     }
 }
