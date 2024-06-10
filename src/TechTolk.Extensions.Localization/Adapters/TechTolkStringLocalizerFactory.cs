9384f9c634ff8e4516477d7733c0f2f364488538
Folkert van der Molen <folkert@fandermill.com>
2024-06-10 22:37:59 +0200

    add tests and fixes for TechTolk.Extensions.Localization

--- src/TechTolk.Extensions.Localization/Adapters/TechTolkStringLocalizerFactory.cs
+++ src/TechTolk.Extensions.Localization/Adapters/TechTolkStringLocalizerFactory.cs
@@ -4,20 +4,21 @@ namespace TechTolk.Extensions.Localization.Adapters;
 
 internal class TechTolkStringLocalizerFactory : IStringLocalizerFactory
 {
-	private ITolkFactory _tolkFactory;
+    private ITolkFactory _tolkFactory;
 
-	public TechTolkStringLocalizerFactory(ITolkFactory tolkFactory)
-	{
-		_tolkFactory = tolkFactory;
-	}
+    public TechTolkStringLocalizerFactory(ITolkFactory tolkFactory)
+    {
+        _tolkFactory = tolkFactory;
+    }
 
-	public IStringLocalizer Create(Type resourceSource)
-	{
-		return new TechTolkStringLocalizer(_tolkFactory.Create(resourceSource));
-	}
+    public IStringLocalizer Create(Type resourceSource)
+    {
+        return new TechTolkStringLocalizer(_tolkFactory.Create(resourceSource));
+    }
 
-	public IStringLocalizer Create(string baseName, string location)
-	{
-		return new TechTolkStringLocalizer(_tolkFactory.Create(baseName + location));
-	}
+    public IStringLocalizer Create(string baseName, string location)
+    {
+        string setName = (!string.IsNullOrWhiteSpace(location) ? location + "." : "") + baseName;
+        return new TechTolkStringLocalizer(_tolkFactory.Create(setName));
+    }
 }
