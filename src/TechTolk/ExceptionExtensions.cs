c94ac2856f91903e0b71bf10fbc1886e780e70ee
Folkert van der Molen <folkert@fandermill.com>
2024-06-25 21:54:05 +0200

    Preserve stack trace upon handling AggregateException in TranslationSetCompiler (#20)

---
+++ src/TechTolk/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TechTolk;
+
+internal static class ExceptionExtensions
+{
+    public static void ThrowWithPreservedStackTrace(this Exception exception)
+    {
+        ThreadStart? savestack = Delegate.CreateDelegate(
+                typeof(ThreadStart), exception, "InternalPreserveStackTrace", false, false)
+                as ThreadStart;
+
+        if (savestack is not null)
+            savestack();
+
+        throw exception;
+    }
+}
