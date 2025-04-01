2701f98fb10a976f8684916d03390ec115a22efb
Folkert van der Molen <folkert@fandermill.com>
2025-04-01 21:52:16 +0200

    Adding a TechTolkBuilderExtensions.UseCultureInfoDividers overload that accepts an array of CultureInfo, include test

--- tests/TechTolk.Tests/ShorthandExtensionMethodTests.cs
+++ tests/TechTolk.Tests/ShorthandExtensionMethodTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using TechTolk.Registration.Builders;
 using TechTolk.Tests.TestTranslationSets;
 using TechTolk.TestSuite.Helpers;
@@ -7,18 +8,40 @@ namespace TechTolk.Tests;
 public class ShorthandExtensionMethodTests : AbstractTechTolkTests
 {
     [Fact]
-    public void Can_use_the_shorthand_extension_methods_as_the_normal_methods_were_used()
+    public void Can_use_the_shorthand_extension_methods_for_strings_as_if_the_normal_methods_were_used()
     {
         _services
             .AddTechTolk()
             .UseCultureInfoDividers("nl-NL", "en-US")
             .AddTranslationSetFromSource<Set1>();
 
-        Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("nl-NL");
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("nl-NL");
         var tolk = GetTolkForTranslationSet<Set1>();
 
         var result = tolk.Translate("MyKey");
 
         result.Should().Be("NL-MyValue");
+    }
+
+    [Fact]
+    public void Can_use_the_shorthand_extension_methods_for_culture_infos_as_if_the_normal_methods_were_used()
+    {
+        var supportedCultures = new[]
+        {
+            new CultureInfo("nl-NL"),
+            new CultureInfo("en-US")
+        };
+
+        _services
+            .AddTechTolk()
+            .UseCultureInfoDividers(supportedCultures)
+            .AddTranslationSetFromSource<Set1>();
+
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        var tolk = GetTolkForTranslationSet<Set1>();
+
+        var result = tolk.Translate("MyKey");
+
+        result.Should().Be("EN-MyValue");
     }
 }
